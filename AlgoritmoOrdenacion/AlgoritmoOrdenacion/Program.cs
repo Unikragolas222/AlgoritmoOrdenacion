f4897760906bce7fce0d8d6b6ebbd73281848730
Unikragolas222 <unai_celaya@euneiz.com>
2024-12-17 08:29:23 +0100

    Added algorithm to the project

--- AlgoritmoOrdenacion/AlgoritmoOrdenacion/Program.cs
+++ AlgoritmoOrdenacion/AlgoritmoOrdenacion/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear lista de cantidades
+// Crear Do while para pedir mas numeros
+// Crear lista de cantidades ordenada
+// Ordenar de mayor a menor cantidad
+
+//Mientras que lista de cantidades tenga elemntos (While)
+
+    // Crear variable de tipo int (maxQuant)
+    // Crear variable de tipo int (maxQuantPos)
+    // Crear variabele indice para recorrer la lista y llamrla "x"    // Aumentar maxQuat del indice a 1
+
+        // Recorrer lista de cantidades con un foreach "quantity"
+        // Sí quantity > maxQuant
+
+            // Actualizar maxQuant con valor de quantity
+            // Guardar posición en maxQuantPos
+
+    // Copiar el elemento de maxQuantPos en ListaOrdenada
+    // Eliminar el elemento de maxQuantPos de ListaOrdenada
+
+//Pedir al usuario orden descendente o ascendente
+// Crear bucle while con la condición de que response != "A/D"
+
+// Crear ListOrdQuant
+
+
+// Mostrar por pantalla la lista ordenada
+
+    // Repetir (con while) hasta que tamaño de ListProdQuant = 0
+        // Crear variable MaxCant e inicializar a -1
+        // Crear variable MaxPos e inicializar a -1
+        // Recorrer ListProdQuant: Contar de 0 a tamaño de ListProdQuant y llamar "x"
+            // Si elemento "x" de ListProdQuant > MaxCant
+                // MaxCant = valor de elemento "x" de ListProdQuant
+                // MaxPos = x
+        // Añadir a ListOrdQuant el elemento MinPos de ListProdQuant
+        // Eliminar elemento MinPos de ListProdQuant
+
+// Mostrar por pantalla la lista ordenada
