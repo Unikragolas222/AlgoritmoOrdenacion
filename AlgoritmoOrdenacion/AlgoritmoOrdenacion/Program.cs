ddc92ff59a613578cf02c0724d2857e790b93a4b
Unikragolas222 <unai_celaya@euneiz.com>
2024-12-17 09:10:23 +0100

    Added the code to the project.

--- AlgoritmoOrdenacion/AlgoritmoOrdenacion/Program.cs
+++ AlgoritmoOrdenacion/AlgoritmoOrdenacion/Program.cs
@@ -1,39 +1,121 @@
 ﻿// Crear lista de cantidades
+List<int> ListaNumeros = new List<int>();
+
+ConsoleKey continuar;
+Console.ForegroundColor = ConsoleColor.Blue;
+
 // Crear Do while para pedir mas numeros
-// Crear lista de cantidades ordenada
-// Ordenar de mayor a menor cantidad
+do
+{
+    Console.Clear();
+    Console.WriteLine("\nIntroduzca el numero entero que quiera:");
+    string respuestaUsuario = Console.ReadLine();
+    int cantProd = int.Parse(respuestaUsuario);
+    ListaNumeros.Add(cantProd);
 
-//Mientras que lista de cantidades tenga elemntos (While)
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\n¿Desea introducir más numeros? (S/N)");
+    continuar = Console.ReadKey().Key;
 
-    // Crear variable de tipo int (maxQuant)
-    // Crear variable de tipo int (maxQuantPos)
-    // Crear variabele indice para recorrer la lista y llamrla "x"    // Aumentar maxQuat del indice a 1
+} while (continuar == ConsoleKey.S);
 
-        // Recorrer lista de cantidades con un foreach "quantity"
-        // Sí quantity > maxQuant
+// Crear lista de numeros ordenada
+List<int> ListaNumerosOrdenada = new List<int>();
 
-            // Actualizar maxQuant con valor de quantity
-            // Guardar posición en maxQuantPos
+Console.Clear();
 
-    // Copiar el elemento de maxQuantPos en ListaOrdenada
-    // Eliminar el elemento de maxQuantPos de ListaOrdenada
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("\n" + "La lista es esta:" + "\n");
+foreach (int numero in ListaNumeros)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(numero + "\n");
+}
+Console.WriteLine();
 
 //Pedir al usuario orden descendente o ascendente
+Console.WriteLine("\n¿Quieres ordenar la lista en orden ascendente (A) o descendente (D)?");
+continuar = Console.ReadKey().Key;
+
 // Crear bucle while con la condición de que response != "A/D"
+while (continuar != ConsoleKey.A && continuar != ConsoleKey.D)
+{
+    Console.WriteLine("\nEsa respuesta no es valida. Por favor haz click en la tecla A/D");
+    continuar = Console.ReadKey().Key;
+}
 
 // Crear ListOrdQuant
-
-
-// Mostrar por pantalla la lista ordenada
-
+if (continuar == ConsoleKey.D)
+{
     // Repetir (con while) hasta que tamaño de ListProdQuant = 0
+    while (ListaNumeros.Count != 0)
+    {
         // Crear variable MaxCant e inicializar a -1
-        // Crear variable MaxPos e inicializar a -1
-        // Recorrer ListProdQuant: Contar de 0 a tamaño de ListProdQuant y llamar "x"
-            // Si elemento "x" de ListProdQuant > MaxCant
-                // MaxCant = valor de elemento "x" de ListProdQuant
-                // MaxPos = x
-        // Añadir a ListOrdQuant el elemento MinPos de ListProdQuant
-        // Eliminar elemento MinPos de ListProdQuant
+        int maxQuant = -1;
 
+        // Crear variable MaxPos e inicializar a -1
+        int maxPos = -1;
+
+        // Recorrer ListProdQuant: Contar de 0 a tamaño de ListProdQuant y llamar "x"
+        for (int x = 0; x < ListaNumeros.Count; x++)
+        {
+            // Si elemento "x" de ListProdQuant > MaxCant
+            if (ListaNumeros[x] > maxQuant)
+            {
+                // MaxCant = valor de elemento "x" de ListProdQuant
+                maxQuant = ListaNumeros[x];
+
+                // MaxPos = x
+                maxPos = x;
+            }
+        }
+
+        // Añadir a ListOrdQuant el elemento MaxPos de ListProdQuant
+        ListaNumerosOrdenada.Add(ListaNumeros[maxPos]);
+
+        // Eliminar elemento MaxPos de ListProdQuant
+        ListaNumeros.RemoveAt(maxPos);
+    }
+}
+else
+{
+    // Repetir (con while) hasta que tamaño de ListProdQuant = 0
+    while (ListaNumeros.Count != 0)
+    {
+        // Crear variable MinCant e inicializar a int.MaxValue
+        int minQuant = int.MaxValue;
+
+        // Crear variable MinPos e inicializar a -1
+        int minPos = -1;
+
+        // Recorrer ListProdQuant: Contar de 0 a tamaño de ListProdQuant y llamar "x"
+        for (int x = 0; x < ListaNumeros.Count; x++)
+        {
+            // Si elemento "x" de ListProdQuant < MinCant
+            if (ListaNumeros[x] < minQuant)
+            {
+                // MinCant = valor de elemento "x" de ListProdQuant
+                minQuant = ListaNumeros[x];
+
+                // MinPos = x
+                minPos = x;
+            }
+        }
+
+        // Añadir a ListOrdQuant el elemento MinPos de ListProdQuant
+        ListaNumerosOrdenada.Add(ListaNumeros[minPos]);
+
+        // Eliminar elemento MinPos de ListProdQuant
+        ListaNumeros.RemoveAt(minPos);
+    }
+}
+
+Console.Clear();
 // Mostrar por pantalla la lista ordenada
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("La lista ordenada quedaría así:\n");
+foreach (int numero in ListaNumerosOrdenada)
+{
+    Console.Write(numero + "\n");
+}
+Console.WriteLine();
